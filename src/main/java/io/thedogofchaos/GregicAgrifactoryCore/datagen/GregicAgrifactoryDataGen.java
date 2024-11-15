d4cd33fb8a5f90bcf2fced8f960cbc214c9760d5
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-11-15 23:11:23 +0000

    man

---
+++ src/main/java/io/thedogofchaos/GregicAgrifactoryCore/datagen/GregicAgrifactoryDataGen.java
@@ -0,0 +1,25 @@
+package io.thedogofchaos.GregicAgrifactoryCore.datagen;
+
+import io.thedogofchaos.GregicAgrifactoryCore.GregicAgrifactoryCore;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.concurrent.CompletableFuture;
+
+@Mod.EventBusSubscriber(modid = GregicAgrifactoryCore.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class GregicAgrifactoryDataGen {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator gen = event.getGenerator();
+        PackOutput packOutput = gen.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        gen.addProvider(event.includeServer(), LootTableProvider.create(packOutput));
+    }
+}
