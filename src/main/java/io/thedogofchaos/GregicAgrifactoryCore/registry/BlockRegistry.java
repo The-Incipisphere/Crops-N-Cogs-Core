4dd9f26bf849d086e7bd09932bfc2aa225d30c8d
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-11-17 14:02:13 +0000

    jdocs & method condensation

--- src/main/java/io/thedogofchaos/GregicAgrifactoryCore/registry/BlockRegistry.java
+++ src/main/java/io/thedogofchaos/GregicAgrifactoryCore/registry/BlockRegistry.java
@@ -13,25 +13,34 @@ import java.util.function.Supplier;
 public class BlockRegistry {
     protected static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, GregicAgrifactoryCore.MOD_ID);
 
+
+    // Helper methods below here.
     /**
-     * An overloaded method to register a block.
+     * An overloaded method to register a block (and automatically, a {@link BlockItem} at the same time).
      * @param name A {@link String} with the name of the block to register.
      * @param block A {@link Supplier} with the block to register.
-     * @return registerBlock(name, block, true)
+     * @return {@code registerBlock(name, block, true)}
+     * @since 1.0.0
      * @param <T> The type of the {@link Supplier}.
      */
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         return registerBlock(name, block, true);
     }
+
+    /**
+     * An overloaded method to register a block (and OPTIONALLY, a {@link BlockItem} at the same time).
+     * @param name A {@link String} with the name of the block to register.
+     * @param block A {@link Supplier} with the block to register.
+     * @param alsoRegisterBlockItem If true, also register a {@link BlockItem}.
+     * @return The {@code registeredBlock}.
+     * @since 1.0.0
+     * @param <T> The type of the {@link Supplier}.
+     */
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, boolean alsoRegisterBlockItem) {
         RegistryObject<T> registeredBlock;
         registeredBlock = BLOCKS.register(name, block);
-        if (alsoRegisterBlockItem) registerBlockItem(name, registeredBlock);
+        if (alsoRegisterBlockItem) ItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
 
         return registeredBlock;
-    }
-
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
-        return ItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
 }
