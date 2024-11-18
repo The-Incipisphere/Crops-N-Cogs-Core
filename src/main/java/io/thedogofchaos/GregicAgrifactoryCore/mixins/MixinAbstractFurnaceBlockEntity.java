9d409916949f60915be7425db0efa30e4e299f2d
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-11-18 16:17:39 +0000

    fuck it, working on furnace mixins

---
+++ src/main/java/io/thedogofchaos/GregicAgrifactoryCore/mixins/MixinAbstractFurnaceBlockEntity.java
@@ -0,0 +1,10 @@
+package io.thedogofchaos.GregicAgrifactoryCore.mixins;
+
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+// Main logic injection.
+@Mixin(AbstractFurnaceBlockEntity.class)
+public class MixinAbstractFurnaceBlockEntity {
+
+}
