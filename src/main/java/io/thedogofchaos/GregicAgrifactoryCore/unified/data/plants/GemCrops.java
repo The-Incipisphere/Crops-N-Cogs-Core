50860c9d9700edda810b6162a57f3cf78103d226
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2025-01-15 15:13:07 +0000

    cleanup

--- src/main/java/io/thedogofchaos/GregicAgrifactoryCore/unified/data/plants/GemCrops.java
+++ src/main/java/io/thedogofchaos/GregicAgrifactoryCore/unified/data/plants/GemCrops.java
@@ -2,7 +2,6 @@ package io.thedogofchaos.GregicAgrifactoryCore.unified.data.plants;
 
 import io.thedogofchaos.GregicAgrifactoryCore.organic.Crop;
 import io.thedogofchaos.GregicAgrifactoryCore.organic.CropTextures;
-import net.minecraft.resources.ResourceLocation;
 
 import static io.thedogofchaos.GregicAgrifactoryCore.GregicAgrifactoryCore.id;
 import static io.thedogofchaos.GregicAgrifactoryCore.unified.data.ModPlants.*;
